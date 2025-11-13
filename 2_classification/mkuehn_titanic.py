b38c8195405159732ed8430b2d845268a7296772
mck30 <mckuehn30@gmail.com>
2025-11-13 15:14:30 -0600

    edits to titanic python file

--- 2_classification/mkuehn_titanic.py
+++ 2_classification/mkuehn_titanic.py
@@ -1,13 +1,23 @@
 import  numpy as np
 import pandas as pd
-import madplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import imblearn.over_sampling as ios
+import seaborn as sns
+
+def predict(titanic_df):
+    titanic_df = titanic_df.dropna()
+    
+    all_independant_vars = titanic_df.columns.drop('Survived').values.tolist()
+    
+    titanic_predictors_df = titanic_df[all_independant_vars]
+    titanic_response_df = titanic_df['Survived']
 
 def main():
     """Main function"""
-    titanic_df = pd.read_csv("Titanic-Dataset.csv")
-    print(titanic_df.head())
+    titanic_df = pd.read_csv("E:/Madison College/Machine Learning/mad-2025-fall-ml-the-algorithms/2_classification/Titanic-Dataset.csv")
+    # print(titanic_df.head())
     
 if __name__ == "__main__":
     main()
